12f6dc236617abe8fe302d4965e61b082870dba3
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-04 12:01:53 -0400

    added checkboxes for search

--- vendor-connector/src/Reviews.js
+++ vendor-connector/src/Reviews.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './Reviews.css'
 
 function Reviews() {
   const [records, updateRecords] = useState([]);
+  const [venueVisible, updateVenueVisible] = useState('visible');
+  const [djVisible, updateDjVisible] = useState('visible');
+  const [photogVisible, updatePhotogVisible] = useState('visible');
+  const [floristVisible, updateFloristVisible] = useState('visible');
 
   // Get the records from Airtable
   useEffect(() => {
@@ -25,11 +30,11 @@ function Reviews() {
   }, []);
 
   // Helper function to display a review
-  function displayRecords(recordInput, vendorVar, vendorReview, vendor) {
+  function displayRecords(recordInput, vendorVar, vendorReview, vendor, vendorVisible) {
     const record = recordInput.fields
     if (record[vendorReview]) {
       return (
-        <div>
+        <div className={`${vendorVisible}`}>
           <h2>{record.name} / <a href={`mailto:${record.email}?subject=Your%20${record[vendorVar]}%20review%20on%20Vendor%20Connector`}>{record.email}</a></h2>
           <h3>{record[vendorVar]} ({vendor})</h3>
           <p>{record[vendorReview]}</p>
@@ -38,15 +43,23 @@ function Reviews() {
     }
   }
 
-  // If records exist, call displayRecords() to display records for each kind of vendor
+  // If records exist, call displayRecords() to display records for each kind of vendor &
   if (records !== []) {return (
-      <div>
+    <div>
+      <label>Venues:<input type='checkbox' id='hideVenue' name='venueCheckbox' value='visible' defaultChecked onChange={e => e.target.checked ? updateVenueVisible('visible') : updateVenueVisible('hidden')} /></label>
+
+      <label> DJs:<input type='checkbox' id='hideDj' name='djCheckbox' value='visible' defaultChecked onChange={e => e.target.checked ? updateDjVisible('visible') : updateDjVisible('hidden')} /></label>
+
+      <label> Photographers:<input type='checkbox' id='hidePhotogs' name='photogsChecked' value='visible' defaultChecked onChange={e => e.target.checked ? updatePhotogVisible('visible') : updatePhotogVisible('hidden')} /></label>
+
+      <label> Florists:<input type='checkbox' id='hideFloriists' name='floristCheckbox' value='visible' defaultChecked onChange={e => e.target.checked ? updateFloristVisible('visible') : updateFloristVisible('hidden')}/></label>
+      
       {records.map(record => (
         <div>
-          {displayRecords(record, 'venue', 'venueReview', 'Venue')}
-          {displayRecords(record, 'dj', 'djReview', 'DJ')}
-          {displayRecords(record, 'photog', 'photogReview', 'Photographer')}
-          {displayRecords(record, 'florist', 'floristReview', 'Florist')}
+          {displayRecords(record, 'venue', 'venueReview', 'Venue', venueVisible)}
+          {displayRecords(record, 'dj', 'djReview', 'DJ', djVisible)}
+          {displayRecords(record, 'photog', 'photogReview', 'Photographer', photogVisible)}
+          {displayRecords(record, 'florist', 'floristReview', 'Florist', floristVisible)}
         </div>
         ))}
       </div>
