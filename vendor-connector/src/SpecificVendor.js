5afd3904cd043110a0d80f4e9b5bfc100ce01e56
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-04 18:49:54 -0400

    when you select a new vendor-type vendor-name should reset to top option - it does not

--- vendor-connector/src/SpecificVendor.js
+++ vendor-connector/src/SpecificVendor.js
@@ -1,58 +1,86 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import DisplayARecord from './DisplayARecord';
 
 function SpecificVendor() {
+  const [vendorTypeSelect, updateVendorTypeSelect] = useState('');
+  const [vendorNameSelect, updateVendorNameSelect] = useState('');
+  const [allRecords, updateAllRecords] = useState([]);
+  const [options, updateOptions] = useState([]);
 
-const [vendorTypeSelect, updateVendorTypeSelect] = useState('')
-const [vendorNameSelect, updateVendorNameSelect] = useState('')
-const [specificRecords, updateSpecificRecords] = useState ([])
-  
-
-// Get the records from Airtable
-useEffect(() => {
-  async function apiGet() {
-    try {
-      const recordsObj = await axios.get(
-        'https://api.airtable.com/v0/appN3Jd5GDuwUR5wG/Table%201?&view=Grid%20view',
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
-          },
-        }
-      );
-      console.log(recordsObj.data.records)
-      updateSpecificRecords(recordsObj.data.records);
-    } catch (err) {
-      console.log(err);
+  // Get the records from Airtable
+  useEffect(() => {
+    async function apiGet() {
+      try {
+        const recordsObj = await axios.get(
+          'https://api.airtable.com/v0/appN3Jd5GDuwUR5wG/Table%201?&view=Grid%20view',
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+            },
+          }
+        );
+        //console.log(recordsObj.data.records)
+        updateAllRecords(recordsObj.data.records);
+      } catch (err) {
+        console.log(err);
+      }
     }
-  }
-  apiGet();
-}, []);
+    apiGet();
+  }, []);
+
+  useEffect(() => {
+    const newOptions = allRecords.reduce((acc, record) => {
+      if (record.fields[vendorTypeSelect] && !acc.includes(record.fields[vendorTypeSelect])) {
+        acc.push(record.fields[vendorTypeSelect]);
+      }
+      return acc;
+    }, []);
+    updateOptions(newOptions);
+  }, [vendorTypeSelect]);
 
   return (
     <div>
-      <select onChange={e => updateVendorTypeSelect(e.target.value)}>
+      <select onChange={(e) => updateVendorTypeSelect(e.target.value)}>
         <option value=''>Select a Vendor Type</option>
         <option value='venue'>Venue</option>
         <option value='dj'>DJ</option>
         <option value='photog'>Photographer</option>
         <option value='florist'>Florist</option>
       </select>
+      {/* 
+      <select onChange={e => updateVendorNameSelect(e.target.value)}> */}
 
-      <select onChange={e => updateVendorNameSelect(e.target.value)}>
-        <option value=''>Select a Specific Vendor</option>
-        {specificRecords.map(record => {
-          return record.fields[vendorTypeSelect] && <option value={record.fields[vendorTypeSelect]}>{record.fields[vendorTypeSelect]}</option>
+      {/* {specificRecords=['Select a Specific Vendor']} */}
+
+      {/* {allRecords.map(record => {
+          if (record.fields[vendorTypeSelect] && !specificRecords.includes(record.fields[vendorTypeSelect])) {
+            specificRecords.push(record.fields[vendorTypeSelect])
+          }
         })}
-      </select>
+        {console.log('specificRecords = ', specificRecords)} */}
 
-      {specificRecords.map(record => (
-        record.fields[vendorTypeSelect] === vendorNameSelect && <DisplayARecord record={record} vendorVar={vendorTypeSelect} vendorReview={`${vendorTypeSelect}Review`} vendor='' vendorVisible='visible' />
-      ))}
+      {/* {specificRecords.map(record => <option value={`${record}`}>{`${record}`}</option>)}
+      </select> */}
 
+      {options.length >= 0 && (
+        <select onChange={(e) => updateVendorNameSelect(e.target.value)}>
+          <option value='' selected>Select a Specific Vendor</option>
+          {options.map((vendor) => {
+            return <option value={vendor}>{vendor}</option>;
+          })}
+        </select>
+      )}
+
+      {/* Displaying records that match criteria */}
+      {allRecords.map(
+        (record) =>
+          record.fields[vendorTypeSelect] === vendorNameSelect && (
+            <DisplayARecord record={record} vendorVar={vendorTypeSelect} vendorReview={`${vendorTypeSelect}Review`} vendor='' vendorVisible='visible' />
+          )
+      )}
     </div>
-  )
+  );
 }
 
-export default SpecificVendor
+export default SpecificVendor;
