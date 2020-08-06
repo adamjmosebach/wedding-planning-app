949d371cccc59a9edf5caf797fbceee569f8b436
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-05 21:18:48 -0400

    fixed underline on home page

--- vendor-connector/src/Home.js
+++ vendor-connector/src/Home.js
@@ -6,8 +6,8 @@ function Home() {
   return (
     <div className='home-div'>
       <div className='home-options'>
-        <Link to='/create'><div className='home-option'><h2 id='create-opt'>Create a Review</h2></div></Link>
-        <Link to='/reviews'><div className='home-option'><h2 id='read-opt'>Read Reviews: Make a Vendor Connection</h2></div></Link>
+        <Link to='/create' className='create-link'><div className='home-option'><h2 id='create-opt'>Create a Review</h2></div></Link>
+        <Link to='/reviews' className='create-link'><div className='home-option'><h2 id='read'>Make a Vendor Connection!</h2></div></Link>
       </div>
     </div>
   )
