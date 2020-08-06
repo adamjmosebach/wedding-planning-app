c26127a751efb1f023c8daa31adf45f564bcd4b3
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-05 22:11:02 -0400

    diamond in logo

--- vendor-connector/src/Nav.js
+++ vendor-connector/src/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.css'
-import logo from './images/logo.png'
+import logo from './images/logo-diamond-full.png'
 
 function Nav() {
   return (
